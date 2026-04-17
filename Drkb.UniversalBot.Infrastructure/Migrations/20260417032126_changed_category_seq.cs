2c3d204387ea9352f7c75de416f98cae4df63462
Meha_meh <57707748+EngMeha@users.noreply.github.com>
2026-04-17 16:25:23 +0800

    changed db

---
+++ Drkb.UniversalBot.Infrastructure/Migrations/20260417032126_changed_category_seq.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Drkb.UniversalBot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changed_category_seq : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Seq",
+                table: "Categories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seq",
+                table: "Categories");
+        }
+    }
+}
---
+++ Drkb.UniversalBot.Infrastructure/Migrations/20260417082335_changed_configure.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Drkb.UniversalBot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changed_configure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Title_CategoryStatus",
+                table: "Categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Title_CategoryStatus",
+                table: "Categories",
+                columns: new[] { "Title", "CategoryStatus" },
+                unique: true);
+        }
+    }
+}
